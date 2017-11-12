9fd106b51f0bc8e268f558cfdc918c25cae990c2
Matthew Smith <mattsheridansmith@gmail.com>
2017-11-12 13:00:54 -0500

    [FIX] Removed previous contracts and tests (capsule).

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,7 @@
 const SimpleStorage = artifacts.require('./SimpleStorage.sol');
-const Capsule = artifacts.require('./Capsule.sol');
 const CacheGame = artifacts.require('./CacheGame.sol');
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
-  deployer.deploy(Capsule);
   deployer.deploy(CacheGame);
 };
