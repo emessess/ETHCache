d3a9b49431a941f932eb37fc470571d235966e63
Matthew Smith <mattsheridansmith@gmail.com>
2017-11-12 17:24:52 -0500

    Removed SimpleStorage examples.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,5 @@
-const SimpleStorage = artifacts.require('./SimpleStorage.sol');
 const CacheGame = artifacts.require('./CacheGame.sol');
 
 module.exports = function(deployer) {
-  deployer.deploy(SimpleStorage);
   deployer.deploy(CacheGame);
 };
